98b1d6515fc2f3947f55202e4cb79c807384d3ba
MerisaAlfionita <merisaalfionita3@gmail.com>
2024-05-22 14:12:45 +0700

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,69 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5rsyYdXcL46":
+        Script1();
+        break;
+      case "6fCZxeiOPEA":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('5vFdkfowYMJ');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6FR3oY1MCle';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5acS2TrbJXm');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5WSPDObtj5d';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
